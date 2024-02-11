4a7c7c7e6fe1aefe4052f3773d25005a73e8235a
swos <szymon.wos1319@gmail.com>
2024-02-12 00:39:40 +0100

    Add map of all projects and functions to it

--- src/model.js
+++ src/model.js
@@ -8,16 +8,27 @@ import Todo from './classes/Todo.js';
 const model = (() => {
 
     let currentProject;
+    const projects = new Map();
 
     const createProject = (title, description) => {
         const project = new Project(title, description);
+        projects.set(project.id, project);
         return project;
-    }
-    const setCurrentProject = (project) => {
-        currentProject = project;
     }
     const updateProject = (project, title, description) => {
         project.setProperties(title, description);
+    }
+    const deleteProject = (projectId) => {
+        projects.delete(projectId);
+    }
+    const getProjects = () => {
+        return projects;
+    }
+    const getCurrentProject = () => {
+        return currentProject;
+    }
+    const setCurrentProject = (projectId) => {
+        currentProject = projects.get(projectId);
     }
     const createTodo = (title, description, priority, dueDate) => {
         const todo = new Todo(title, description, priority, dueDate);
@@ -31,7 +42,7 @@ const model = (() => {
         currentProject.removeTodo(todo);
     }
     
-    return { createProject, setCurrentProject, updateProject, createTodo, updateTodo, deleteTodo }
+    return { createProject, updateProject, deleteProject, getProjects, getCurrentProject, setCurrentProject, createTodo, updateTodo, deleteTodo }
 })();
 
 export default model;
